3beb7be124f2b5ac8e20e3fe2c907a9305d48ab9
kauar32580-del <kauar32580@gmail.com>
2026-05-28 16:20:52 +0000

    Nome do Arquivo

---
+++ Questao3.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Questao3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite uma letra de A a J: ");
+        char letra = sc.next().toUpperCase().charAt(0);
+        int pontos;
+        if (letra == 'A') {
+            pontos = 10;
+        } else if (letra == 'B') {
+            pontos = 20;
+        } else if (letra == 'C') {
+            pontos = 30;
+        } else if (letra == 'D') {
+            pontos = 40;
+        } else if (letra == 'E') {
+            pontos = 50;
+        } else if (letra == 'F') {
+            pontos = 60;
+        } else if (letra == 'G') {
+            pontos = 70;
+        } else if (letra == 'H') {
+            pontos = 80;
+        } else if (letra == 'I') {
+            pontos = 90;
+        } else if (letra == 'J') {
+            pontos = 100;
+        } else {
+            System.out.println("Letra fora do intervalo permitido!");
+            sc.close();
+            return;
+        }
+        System.out.println("Você ganhou " + pontos + " pontos!");
+        if (pontos >= 80) {
+            System.out.println("Excelente escolha!");
+        } else if (pontos >= 50) {
+            System.out.println("Boa escolha!");
+        } else if (pontos >= 30) {
+            System.out.println("Poderia ser melhor...");
+        } else {
+            System.out.println("Tente novamente!");
+        }
+        sc.close();
+    }
+}
+
